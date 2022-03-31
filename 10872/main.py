7619077fb4e978ee08f237c9de7000fb5d4ac4e0
yeomja99 <sieweke99@gmail.com>
2022-03-31 20:08:04 +0900

    BOJ_10872

---
+++ 10872/main.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def factorial(n):
+    if (n==0):
+        return 1
+    if (n==1):
+        return 1
+    return n * factorial(n-1)
+
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    print(factorial(n))
+
